d06f3bba241fb2d8655ddf21f6c5b00269423a53
Topher <chris@matthieu.us>
2010-11-15 19:02:39 -0700

    forms seem to be working

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -1,110 +1,17 @@
-class URI::HTTP
-    def +(new_loc)
-        URI.parse(
-            case new_loc
-              when /^[\/]/    then "http://#{host}:#{port}#{new_loc}"
-              when /^.{3,6}:/ then new_loc
-              else                 "http://#{host}:#{port}#{path.sub(/\/?[^\/]*$/,'/' + new_loc)}"
-              end
-            )
-        end
-    end
-    
-class Net::HTTPResponse
-    def after_redirection_from(uri)
-        self
-    end
-end
-
-class Net::HTTPRedirection
-    def after_redirection_from(uri)
-        Net::HTTP.get_response(uri + self['location']).after_redirection_from(uri) rescue ""
-    end
-end
-
-class String
-    def prefixes(delimiter='/')
-        segments = split(delimiter)
-        (1..segments.length).collect { |i| segments[0..-i].join(delimiter) }
-        end
-    def suffixes(delimiter='/')
-        segments = split(delimiter)
-        (1..segments.length).collect { |i| segments[(i-1)..-1].join(delimiter) }
-        end
-    end
-
-class A_cookie_jar
-    def initialize
-        @jar = {}
-        end
-    def parse_cookie_from(uri,s)
-        return unless s.is_a? String and s.length > 2
-        cookies = s.gsub(/, ([^\d])/,';;\1').split(';;')
-        cookies.each { |cookie|
-            name_value,*options = cookie.split(';')
-            name,value = name_value.split('=')
-            acceptable_protocols,domain,path,expires = ['http','https'],uri.host,uri.path,nil
-            options.each { |option| case option
-                when /expires=.+?, (..-...-..(..)? ..:..:.. GMT)/
-                    expires = DateTime.parse($1,:guess_year)
-                when /path=(.*)/                     
-                    path = $1
-                when /domain=(.*)/                      
-                    domain = $1
-                when /secure/                                
-                    acceptable_protocols = ['https']
-                end}
-            ((@jar[domain] ||= {})[path] ||= []) << [name,value,expires, acceptable_protocols]
-            }
-        end
-    def cookies_for(uri)
-        result = {}
-        uri.host.suffixes('.').each { |domain|
-            uri.path.prefixes.each { |path|
-                @jar[domain][path].each { |name,value,expires,acceptable_protocols|
-                    (result[name] ||= []) << value if DateTime.now < expires and acceptable_protocols.include? uri.scheme
-                    } if @jar[domain].has_key? path
-                path += '/'    
-                } if @jar.has_key? domain
-            }
-            
-          def result.to_s
-          keys.collect { |name|
-              self[name].collect { |value| "#{name}=#{value}" }
-              }.flatten.join(';')
-          end
-        result
-        end
-    end
-
-
 class ProxyController < ApplicationController
   
-  def cookie_jar
-    session[:cookie_jar] ||= A_cookie_jar.new
-  end
-  
-  def fetch(method,uri,data)
-    headers = {'Cookie' => cookie_jar.cookies_for(uri).to_s } 
-    args    = (method == 'POST' || method == 'PUT') ? [ data, headers ] : [ headers ]
-    response = Net::HTTP.start(uri.host,uri.port) { |x| x.send(method.downcase,[uri.path,uri.query].join('?'), args) } 
-    cookie_jar.parse_cookie_from(uri,response['set-cookie']) 
-    response = fetch('GET',uri + response['location'],data) if response.is_a? Net::HTTPRedirection
-    response
-  end
-
   def proxy
     
-    site_url =  request.env["REQUEST_URI"][0..request.env["REQUEST_URI"].index("/proxy")-1] #"http://localhost:3000"
-    @url   = URI.unescape(request.env["QUERY_STRING"][4..request.env["QUERY_STRING"].length]) #Full URL (url=http://...)
+    site_url =  request.env["REQUEST_URI"][0..request.env["REQUEST_URI"].index("/proxy")-1] # prefix i.e. "http://localhost:3000"
+    if params[:url].blank?
+      @url   = URI.unescape(request.env["QUERY_STRING"][4..request.env["QUERY_STRING"].length]) #Full URL from querystring url= "http://..."
+    else
+      @url = params[:url]
+    end
     method = request.env["REQUEST_METHOD"]
     data   = request.env["RAW_POST_DATA"] #if empty add the actual querystring to data
-    port   = 80        
-    
-    # if method.nil?
-    #   method = "GET"
-    # end
-        
+    port   = 80               
+                        
     # Prepend http/https protocol if not present
     if @url.index('http://') == nil and @url.index('https://') == nil
       @url = 'http://' + @url
@@ -123,9 +30,9 @@ class ProxyController < ApplicationController
     if @xurl[-1,1] != "/"
       @xurl=@xurl + "/"
     end
-    if @baseurl[-1,1] != "/"
-      @baseurl=@baseurl + "/"
-    end
+    # if @baseurl[-1,1] != "/"
+    #   @baseurl=@baseurl + "/"
+    # end
     
     # determines querystring i.e. http://example.com/subsirectory?data=this_stuff
     iOffset = @url.rindex('?')
@@ -141,29 +48,6 @@ class ProxyController < ApplicationController
     
     host = @uri.host #only domain
 
-    
-    
-    # FETCH PAGE
-    # # Works - but doesnt support cookies
-    # response = Net::HTTP.start(host,port) { |x| 
-    #     x.send(method.downcase,path,data) 
-    #     }    
-    # @rawdoc = response.after_redirection_from(@uri).body rescue ""
-
-        
-    ## Cookie Support - doesn't work    
-    # response = fetch(method, URI.parse(@url), data)           
-    # @rawdoc = response.body
-
-
-    ## Cookie Support - doesn't work    
-    # headers = {'Cookie' => cookie_jar.cookies_for(@uri).to_s } 
-    # args    = (method == 'POST' || method == 'PUT') ? [ data, headers ] : [ headers ]
-    # response = Net::HTTP.start(@uri.host,@uri.port) { |x| x.send(method.downcase, [path, @query].join('?'), args) } 
-    # cookie_jar.parse_cookie_from(@uri, response['set-cookie']) 
-    # response = fetch('GET', @uri + response['location'],data) if response.is_a? Net::HTTPRedirection     
-    # @rawdoc = response.body
-    
     
     a = Mechanize.new { |agent|
       agent.user_agent_alias = 'Mac Safari'
@@ -278,29 +162,13 @@ class ProxyController < ApplicationController
     
           a['action'] = link     
 
-          ##ADD HIDDEN FIELDS TO TRACK VERB, ETC.
-          # lnk_node = Nokogiri::XML::Node.new('input', a)
-          # lnk_node['type'] = 'hidden'
-          # lnk_node['name'] = 'lnk'
-          # lnk_node['value'] = formaction
+          # Add hidden input text form with url
+          lnk_node = Nokogiri::XML::Node.new('input', a)
+          lnk_node['type'] = 'hidden'
+          lnk_node['name'] = 'url'
+          lnk_node['value'] = URI.escape(formaction.strip)
           # a.add_child(lnk_node)
-          # 
-          # if a['method'].nil? or a['method'].downcase == 'get'
-          #   # a.add_child '<input type=hidden name=verb value=get>'
-          #   verb_node = Nokogiri::XML::Node.new('input', a)
-          #   verb_node['type'] = 'hidden'
-          #   verb_node['name'] = 'verb'
-          #   verb_node['value'] = 'get'
-          #   a.add_child(verb_node)
-          # else
-          #   a['method'] = 'get'
-          #   # a.add_child '<input type=hidden name=verb value=post>'
-          #   verb_node = Nokogiri::XML::Node.new('input', a)
-          #   verb_node['type'] = 'hidden'
-          #   verb_node['name'] = 'verb'
-          #   verb_node['value'] = 'post'
-          #   a.add_child(verb_node)
-          # end
+          a.add_child(lnk_node)
 
         end
       end
@@ -321,10 +189,10 @@ class ProxyController < ApplicationController
     # FIX JAVASCRIPT RELEATIVE URLS
     # @finaldoc.gsub("script('/", "script('" + @baseurl)
 
-    #Hack to remove double wacks in URL ie http://sunsounds.org//audio//programs
-    @finaldoc.gsub("://", "/::")
-    @finaldoc.gsub("//", "/")
-    @finaldoc.gsub("/::", "://")         
+    # #Hack to remove double wacks in URL ie http://sunsounds.org//audio//programs
+    # @finaldoc.gsub!("://", "/::")
+    # @finaldoc.gsub!("//", "/")
+    # @finaldoc.gsub!("/::", "://")         
     
     #Add baseURL to code within embedded styles
     @finaldoc.gsub("url(/", "url(" + @baseurl)  
